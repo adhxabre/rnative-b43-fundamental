fb342d01801f04a0e06ae46a751d4a9f1bae2f3b
adhxabre <abeldustin06@gmail.com>
2023-02-09 11:40:56 +0700

    FlatList

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { FlatList, View, Text } from "react-native";
 
-const cars = ["BMW", "Mercedez", "Ferarri", "Honda"];
+const data = [
+  { name: "Abel Dustin", email: "abeldustin06@gmail.com" },
+  { name: "Cintara Surya", email: "suryaelz@gmail.com" },
+  { name: "Dandi Saputra", email: "dandisapoetra@gmail.com" },
+];
 
 export default function List() {
   return (
     <View>
       <Text>This is list on React Native</Text>
 
-      {cars.map((car) => (
-        <Text key={car}>{car}</Text>
-      ))}
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <Text>
+            {item.name} | {item.email}
+          </Text>
+        )}
+        keyExtractor={(item) => item.name}
+      />
     </View>
   );
 }
