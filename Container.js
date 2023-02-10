25a84174ec13983beab9c4ecb78481b37eb41220
adhxabre <abeldustin06@gmail.com>
2023-02-10 09:16:11 +0700

    React Navigation Prepare

--- Container.js
+++ Container.js
@@ -3,6 +3,7 @@ import { Text, Box } from "native-base";
 
 import Hello from "./src/components/Hello";
 import FormNativeBase from "./src/components/FormNativeBase";
+import IncDec from "./src/components/IncDec";
 
 export default function Container() {
   return <FormNativeBase />;
